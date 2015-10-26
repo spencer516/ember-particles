5aeee8d683e7455f58443329fed5365c627eec41
Spencer <spencer@ravellaw.com>
2015-10-26 13:22:51 -0700

    setup initial project

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 /* jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'ember-particles'
+  name: 'ember-particles',
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/particles.js/particles.js');
+  }
 };
