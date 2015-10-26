e31e038f71cfd2104ce377c3bc2f30eb730a1bd2
Spencer <spencer@ravellaw.com>
2015-10-26 13:57:49 -0700

    make sure pJS is destroyed.

--- addon/components/ember-particles/component.js
+++ addon/components/ember-particles/component.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 const {get, on} = Ember;
 
-/* global particlesJS */
+/* global pJS */
 export default Ember.Component.extend({
   classNames: ['ember-particles'],
   _hasElement: false,
+  _particles: null,
 
   _setupHasElement: on('didInsertElement', function() {
     this._hasElement = true;
@@ -13,11 +14,50 @@ export default Ember.Component.extend({
 
   didReceiveAttrs() {
     if(this._hasElement) {
-      this.updateParticles()
+      this.updateParticles();
+    }
+  },
+
+  destroyParticles() {
+    let oldParticles = this._particles;
+    if(oldParticles) {
+      destroyParticles(oldParticles);
+      this._particles = null;
     }
   },
 
   updateParticles() {
-    particlesJS(get(this, 'element.id'), this.getAttr('options'));
+    let element = get(this, 'element');
+
+    // Destroy the old particles instance if exists.
+    this.destroyParticles();
+
+    removeChildren(element);
+    createChildCanvas(element);
+
+    this._particles = new pJS(element.id, this.getAttr('options'));
   }
 });
+
+function removeChildren(node) {
+  while(node.firstChild) {
+    let child = node.firstChild;
+    node.removeChild(child);
+  }
+}
+
+/*
+  We can't use the particlesJS hook to generate a canvas as it doesn't
+  return an instance of the pJS object. So, we'll do it manually.
+ */
+function createChildCanvas(node) {
+  let canvasElement = document.createElement('canvas');
+  canvasElement.className = 'particles-js-canvas-el';
+  canvasElement.style.width = '100%';
+  canvasElement.style.height = '100%';
+  node.appendChild(canvasElement);
+}
+
+function destroyParticles(particles) {
+  particles.pJS.fn.vendors.destroypJS();
+}
--- tests/integration/components/ember-particles/component-test.js
+++ tests/integration/components/ember-particles/component-test.js
@@ -6,21 +6,30 @@ moduleForComponent('ember-particles', 'Integration | Component | ember particles
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{ember-particles}}`);
+  assert.equal(this.$('canvas').length, 1, 'particles was initialized');
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{ember-particles
+    options=(ember-particles-options
+      'particles.number.value' 10
+      'particles.size.value' 3
+    )
+  }}`);
+  assert.equal(this.$('canvas').length, 1, 'particles was initialized with options');
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#ember-particles}}
-      template block text
-    {{/ember-particles}}
-  `);
+test('it destroys and recreates canvas on option change', function(assert) {
+  this.set('numParticles', 10);
+  this.render(hbs`{{ember-particles
+    options=(ember-particles-options
+      'particles.number.value' numParticles
+      'particles.size.value' 3
+    )
+  }}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let ogCanvas = this.$('canvas')[0];
+  this.set('numParticles', 15);
+  assert.ok(ogCanvas !== this.$('canvas')[0], 'a new canvas was created');
 });
